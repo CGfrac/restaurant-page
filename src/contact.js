d906f189144222daf32cfb418f92ef5a6ace8c08
CGfrac <CGfrac@protonmail.com>
2022-06-12 15:25:07 +0200

    Implement createContact

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,15 @@
 function createContact() {
-    return document.createElement('div');
+    const mainDiv = document.createElement('div');
+
+    const title = document.createElement('h1');
+    title.textContent = 'Want to tell us something?';
+
+    const para = document.createElement('p');
+    para.textContent = 'You can find us on Twitter!';
+
+    mainDiv.append(title, para);
+
+    return mainDiv;
 }
 
 export default createContact;
