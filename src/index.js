523bcc641683104263048a5065c07569ba996928
CGfrac <CGfrac@protonmail.com>
2022-06-12 14:50:48 +0200

    Refactor index with module pattern

--- src/index.js
+++ src/index.js
@@ -4,65 +4,75 @@ import createContact from './contact.js';
 
 const content = document.getElementById('content');
 
-const nav = document.createElement('nav');
-const list = document.createElement('ul');
+const displayController = (() => {
+    const _clearPage = () => {
+        const nav = document.querySelector('nav');
+        while (content.lastElementChild !== nav) {
+            content.removeChild(content.lastElementChild);
+        }
+    };
 
-function clearPage() {
-    while (content.lastElementChild !== nav) {
-        content.removeChild(content.lastElementChild);
+    const _updateNav = button => {
+        const className = 'active-tab';
+        const previousButton = document.querySelector('.active-tab');
+        previousButton.classList.remove(className);
+        button.classList.add(className);
+    };
+
+    const _updatePage = e => {
+        const button = e.target;
+
+        _clearPage();
+
+        const tabName = button.textContent;
+
+        let page;
+        switch(tabName) {
+            case 'Home':
+                page = createHome;
+                break;
+            case 'Menu':
+                page = createMenu;
+                break;
+            case 'Contact':
+                page = createContact;
+        }
+
+        content.appendChild(page());
+        _updateNav(button);
+    };
+
+    const _createListElement = content => {
+        const listElement = document.createElement('li');
+        listElement.textContent = content;
+        return listElement;
     }
-}
 
-function updateNav(button) {
-    const className = 'active-tab';
-    const previousButton = document.querySelector('.active-tab');
-    previousButton.classList.remove(className);
-    button.classList.add(className);
-}
+    const generatePage = () => {
+        const nav = document.createElement('nav');
+        const list = document.createElement('ul');
+        const tabNames = ['Home', 'Menu', 'Contact'];
 
-function updatePage(e) {
-    const button = e.target;
+        for (const tabName of tabNames) {
+            const listElement = _createListElement(tabName);
+            if (tabName === 'Home') {
+                listElement.classList.add('active-tab');
+            } else if (tabName === 'Menu') {
+                listElement.classList.add('middle-tab');
+            }
+            listElement.addEventListener('click', _updatePage);
+            list.appendChild(listElement);
+        }
 
-    clearPage();
+        nav.appendChild(list);
+        content.appendChild(nav);
 
-    const tabName = button.textContent;
+        content.appendChild(createHome());
+    };
 
-    let page;
-    switch(tabName) {
-        case 'Home':
-            page = createHome;
-            break;
-        case 'Menu':
-            page = createMenu;
-            break;
-        case 'Contact':
-            page = createContact;
-    }
+    return {
+        generatePage,
+    };
+})();
 
-    content.appendChild(page());
-    updateNav(button);
-}
-
-function createListElement(content) {
-    const listElement = document.createElement('li');
-    listElement.textContent = content;
-    return listElement;
-}
-
-const tabNames = ['Home', 'Menu', 'Contact'];
-
-for (const tabName of tabNames) {
-    const listElement = createListElement(tabName);
-    if (tabName === 'Home') {
-        listElement.classList.add('active-tab');
-    } else if (tabName === 'Menu') {
-        listElement.classList.add('middle-tab');
-    }
-    listElement.addEventListener('click', updatePage);
-    list.appendChild(listElement);
-}
-
-nav.appendChild(list);
-content.appendChild(nav);
-
-content.appendChild(createHome());
+displayController.generatePage();
